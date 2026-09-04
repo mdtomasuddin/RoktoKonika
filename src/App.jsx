2d24c409ec54bfd65945520a02cf75c759c49afb
mdtomasuddin <mdtomasuddin1@gmail.com>
2026-09-04 22:21:57 +0600

    event handler

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,22 @@
-import React from 'react';
+
 
 const App = () => {
+
+  //onClick event handler
+  const onChange = () => {
+    alert('Hello World')
+  }
+
+  //sum
+  const sum = (a, b) => {
+    return a + b
+  }
+
   return (
     <div>
-      <h1>Welcome React</h1>
+      <h1>My First React App</h1>
+      <button onClick={() => alert('Sum is: ' + sum(5, 10))}>Sum</button>
+      <button onClick={onChange}>Click </button>
     </div>
   );
 };
